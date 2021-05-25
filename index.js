e9db9e7cb3697eaa44faff528532119f84a98811
Sanjay K John <sanjay@bestdocapp.com>
2021-05-25 18:55:55 +0530

    Mail template changes

--- index.js
+++ index.js
@@ -1,26 +1,126 @@
 const express = require('express');
 const mjml2html = require('mjml');
-// const handlebars = require('express-handlebars');
 const handlebars = require('handlebars');
 const hb = require('./template');
-// import hbsTemplate from './template';
 const app = express();
 const port = 3000;
 app.set('view engine', 'html');
-// app.set('view engine', 'handlebars');
-// app.engine(
-// 	'handlebars',
-// 	handlebars({
-// 		layoutsDir: __dirname + '/views/layouts'
-// 	})
-// );
 app.use(express.static('public'));
+handlebars.registerHelper('formatDate', function (isoDate) {
+	date = new Date(isoDate);
+	return `${date.toLocaleDateString(
+		'en-US'
+	)} ${date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}`;
+});
 
 const template = handlebars.compile(hb.hbsTemplate);
-const requestData = {
-	id: 1,
-	service: 'Sample Request'
+
+const colorPattern = [
+	{
+		bgColor: '#009FD1'
+	},
+	{
+		bgColor: '#D100BC'
+	},
+	{
+		bgColor: '#00B628'
+	},
+	{
+		bgColor: '#EA7101'
+	}
+];
+let requestData = {
+	assignee: {
+		id: 'WZo6YLJNmg',
+		name: 'sanjay_admin',
+		mobile: '9562850875',
+		userType: 'department-admin'
+	},
+	assignor: {
+		id: 'WZo6YLJNmg',
+		name: 'sanjay_admin',
+		mobile: '9562850875',
+		userType: 'department-admin'
+	},
+	cancelledAt: null,
+	cancelledUser: null,
+	comments: [
+		{
+			commenter: {
+				id: 'WZo6YLJNmg',
+				name: 'sanjay_admin',
+				mobile: '9562850875',
+				userType: 'department-admin'
+			},
+			comments: 'note-3',
+			createdAt: '2021-05-25 13:31:45.355+05:30',
+			id: '09XN2ZMNqb'
+		},
+		{
+			commenter: {
+				id: 'WZo6YLJNmg',
+				name: 'sanjay_admin',
+				mobile: '9562850875',
+				userType: 'department-admin'
+			},
+			comments: 'note-2',
+			createdAt: '2021-05-25 13:31:45.355+05:30',
+			id: '09XN2ZMNqb'
+		},
+		{
+			commenter: {
+				id: 'WZo6YLJNmg',
+				name: 'sanjay_admin',
+				mobile: '9562850875',
+				userType: 'department-admin'
+			},
+			comments: 'note-1',
+			createdAt: '2021-05-25 13:31:45.355+05:30',
+			id: '09XN2ZMNqb'
+		}
+	],
+	completedAt: null,
+	createdAt: '2021-05-14 20:21:20.564+05:30',
+	department: { id: 'KWmND96o17', name: 'House Keeping' },
+	eta: '2021-05-14 21:31:20.563+05:30',
+	events: [],
+	id: 'xwZG19XGY5',
+	location: { id: 'KWmND96o17', name: 'Location 1' },
+	minutesLeft: -15311,
+	minutesSinceCreation: 15381,
+	notes: null,
+	request: 'service-esc-1',
+	requestCompletedBy: null,
+	requestId: 'BSTKAA0703',
+	requestedAt: '2021-05-14T14:51:20.564Z',
+	requestedBy: {
+		id: 'WZjGeD694d',
+		name: 'dept_admin',
+		mobile: '9847330710',
+		userType: 'department-admin'
+	},
+	requestedMobile: '',
+	service: { id: 'Zx4NwQjGQJ', label: 'service-esc-1' },
+	status: 'assigned',
+	unit: {
+		id: '8q6JqxAN2Y',
+		name: 'asd1',
+		status: 'vacant',
+		room: { id: 'gxyGxdbGkB', name: 'asd2' }
+	}
 };
+
+requestData.isDelayed =
+	requestData.status !== 'completed' &&
+	requestData.status !== 'cancelled' &&
+	requestData.minutesLeft &&
+	requestData.minutesLeft <= 0;
+
+requestData.comments.forEach((element, index) => {
+	requestData.comments[index].bgColor =
+		colorPattern[index % colorPattern.length].bgColor;
+	requestData.comments[index].avatarChar = element.commenter.name.charAt(0);
+});
 const options = {};
 const mjml = template(requestData);
 const htmlRow = mjml2html(mjml, options);
--- template.js
+++ template.js
@@ -1,33 +1,135 @@
 exports.hbsTemplate = `
-<mjml version="3.3.3">
-  <mj-body background-color="#F4F4F4" color="#55575d" font-family="Arial, sans-serif">
-    <mj-section background-color="#C1272D" background-repeat="repeat" padding="20px 0" text-align="center" vertical-align="top">
-      <mj-column>
-        <mj-image align="center" padding="10px 25px" src="http://gkq4.mjt.lu/img/gkq4/b/18rxz/1h3k4.png" width="128px"></mj-image>
+<mjml>
+  <mj-head>
+    <mj-breakpoint width="320px" />
+  </mj-head>
+  <mj-body>
+    <mj-wrapper>
+      <mj-section>
+        <mj-column>
+          <mj-text>
+            Hi,
+          </mj-text>
+          <mj-text line-height="20px">The following request is escalated to you. Click on the below button to open it in the BestDoc Concierge (files uploaded to this request will be available in BD Concierge only).
+          </mj-text>
+        </mj-column>
+      </mj-section>
+      <mj-section>
+        <mj-column>
+          <mj-button width="300px" href="http://localhost:8000/requests/xwZG19XGY5" target="__blank" font-family="Helvetica" background-color="#2753bb" color="white">
+            Go to Request
+          </mj-button>
+        </mj-column>
+      </mj-section>
+    </mj-wrapper>
+    <mj-wrapper background-color="#f5f5f5" border-radius="10px" padding="0px 20px 20px 20px">
+      <mj-section>
+        <mj-column>
+          <mj-image width="75px" src="https://apiconcierge.staging.bestdocapp.com/static/bestdoc.svg" />
+          <mj-text align="center" padding="0px">
+            <h1>
+              BD Concierge
+            </h1>
+          </mj-text>
+        </mj-column>
+      </mj-section>
+      <mj-section padding="0px">
+        <mj-column>
+          <mj-text font-size="22px" align="center" font-weight="100" padding="4px">
+            <span style="font-weight: 100;"> {{requestId}} : </span>
+            <span style="font-weight: 600;"> {{request}} </span>
+          </mj-text>
+        </mj-column>
+      </mj-section>
+
+      <mj-section padding="0px 0px 15px 0px">
+        <mj-column>
+          <mj-text align="center">
+            {{#if isDelayed}}
+              <span style="background-color: #f44336; color: #fff; border-radius: 20px; padding: 6px 16px;">Delayed</span>
+            {{/if}}
+          </mj-text>
+        </mj-column>
+      </mj-section>
+
+      <mj-section background-color="#fff" border-radius="20px">
+        <mj-column>
+          <mj-text font-size="16px" align="center" color="#909090" padding="4px">
+            <span style="font-size: 16px; color: #909090;"> Room: </span>
+            <span style="font-size: 18px; color: #000;"> {{unit.room.name}} </span>
+          </mj-text>
+        </mj-column>
+        <mj-column>
+          <mj-text font-size="16px" align="center" color="#909090" padding="4px">
+            <span style="font-size: 16px; color: #909090;"> Unit: </span>
+            <span style="font-size: 18px; color: #000;"> {{unit.name}} </span>
+          </mj-text>
+        </mj-column>
+      </mj-section>
+
+      <mj-section>
+        <mj-column>
+          {{#if assignee}}
+            <mj-text font-size="16px" align="center" color="#909090">
+              Assigned To : {{assignee.name}} ( <a href="tel:{{assignee.mobile}}">{{assignee.mobile}}</a> ) at ____
+            </mj-text>
+          {{/if}}
+          {{#if requestedBy}}
+            <mj-text font-size="16px" align="center" color="#909090">
+              Requested By : {{requestedBy.name}} ( <a href="tel:{{requestedBy.mobile}}">{{requestedBy.mobile}}</a> ) at {{formatDate createdAt}}
+            </mj-text>
+          {{else}}
+            <mj-text font-size="16px" align="center" color="#909090">
+              Requested By : QR Scanned ( <a href="tel:{{requestedMobile}}">{{requestedMobile}}</a> ) at {{formatDate createdAt}}
+            </mj-text>
+          {{/if}}
+        </mj-column>
+      </mj-section>
+
+      {{#if comments}}
+        <mj-section background-color="#fff" border-radius="10px">
+          <mj-column width="100%">
+            <mj-text font-size="16px" align="left">
+              Requested Notes:
+            </mj-text>
+            <mj-divider border-width="1px" border-style="solid" border-color="lightgrey" />
+          </mj-column>
+          {{#each comments as | comment |}}
+            <mj-column width="15%" border-radius="50%">
+              <mj-text color="#fff" background-color="rgb(0, 159, 209)" font-size="22px" align="center" padding="20px 0px 0px 15px" height="25px">
+                <span style="background: {{comment.bgColor}}; padding: 8px 18px 12px 18px; border-radius: 50%;">{{comment.avatarChar}}</span>
+              </mj-text>
+            </mj-column>
+            <mj-column width="85%">
+              <mj-text font-size="14px" align="left">
+                <strong>{{comment.commenter.name}} </strong>- {{formatDate comment.createdAt}}
+              </mj-text>
+              <mj-text font-size="14px" align="left" font-weight="100" padding-top="0px">
+                {{comment.comments}}
+              </mj-text>
+              <mj-divider border-width="1px" border-style="solid" border-color="lightgrey" />
+            </mj-column>
+          {{/each}}
+        </mj-section>
+      {{/if}}
+    </mj-wrapper>
+
+    <mj-section padding-bottom="0px">
+      <mj-column width="100%" padding-bottom="0px">
+        <mj-text font-size="14px" color="#909090" align="center" >
+          You are getting this mail because you are part of the “<Escalation_Name> escalation matrix.
+            To unsubscribe, ask your admin to remove you from this escalation matrix.
+        </mj-text>
+        <mj-divider border-width="1px" border-style="solid" border-color="lightgrey" />
       </mj-column>
     </mj-section>
-    <mj-section background-color="#ffffff" background-repeat="repeat" padding="20px 0" text-align="center" vertical-align="top">
-      <mj-column>
-        <mj-image align="center" padding="10px 25px" src="http://gkq4.mjt.lu/img/gkq4/b/18rxz/1h3s5.gif" width="600px"></mj-image>
-        <mj-image align="center" alt="Happy New Year!" container-background-color="#ffffff" padding="10px 25px" src="http://gkq4.mjt.lu/img/gkq4/b/18rxz/1hlvp.png" width="399px"></mj-image>
+
+    <mj-section>
+      <mj-column width="100%">
+        <mj-image width="200px" href="https://www.bestdocapp.com/" target="__blank" align="center" src="https://apiconcierge.dev.bestdocapp.in/static/poweredByBestdoc.png" />
       </mj-column>
     </mj-section>
-    <mj-section background-color="#ffffff" background-repeat="repeat" background-size="auto" padding="20px 0px 20px 0px" text-align="center" vertical-align="top">
-      <mj-column>
-        <mj-text align="center" color="#55575d" font-family="Arial, sans-serif" font-size="14px" line-height="28px" padding="0px 25px 0px 25px">New dreams, new hopes, new experiences and new joys, we wish you all the best for this New Year to come in 2018!</mj-text>
-        <mj-image align="center" alt="Best wishes from all the Clothes Team!" padding="10px 25px" src="http://gkq4.mjt.lu/img/gkq4/b/18rxz/1hlv8.png" width="142px"></mj-image>
-      </mj-column>
-    </mj-section>
-    <mj-section background-color="#C1272D" background-repeat="repeat" padding="20px 0" text-align="center" vertical-align="top">
-      <mj-column>
-        <mj-text align="center" color="#ffffff" font-family="Arial, sans-serif" font-size="13px" line-height="22px" padding="10px 25px">Simply created&nbsp;on&nbsp;<a style="color:#ffffff" href="http://www.mailjet.com"><b>Mailjet Passport</b></a></mj-text>
-      </mj-column>
-    </mj-section>
-    <mj-section background-repeat="repeat" background-size="auto" padding="20px 0px 20px 0px" text-align="center" vertical-align="top">
-      <mj-column>
-        <mj-text align="center" color="#55575d" font-family="Arial, sans-serif" font-size="11px" line-height="22px" padding="0px 20px">{{service}}</mj-text>
-      </mj-column>
-    </mj-section>
+
   </mj-body>
 </mjml>
-		  `;
+`;
